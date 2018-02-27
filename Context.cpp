644965ab829b2b2ca03313c351639d74866b2dd0
Grant Gadomski <gg2727@rit.edu>
2018-02-26 22:19:14 -0500

    Implemented backup for Context.

--- Context.cpp
+++ Context.cpp
@@ -1,25 +1,24 @@
 #include "Context.hpp"
 #include "State.hpp"
 #include "Transition.hpp"
-#include "Globals.hpp"
 
 #include <iostream>
 #include <string>
 #include <map>
+#include <queue>
 #include <unistd.h>
 #include <pthread.h>
 using namespace std;
-
-bool BUTTON_PRESSED = false;
-bool OVERCURRENT = false;
-bool INFRARED_BEAM = false;
 
 bool Context::motorUp = false;
 bool Context::motorDown = false;
 bool Context::infraredBeam = false;
 int Context::position = 0;
 bool Context::toRaise = true;
+
 StateTable Context::stateTable = StateTable();
+
+queue<Event> contextQueue = new queue<Event>();
 
 Context::Context() {}
 
@@ -31,6 +30,7 @@ void Context::setupStateTable() {
 	stateClosedTransitionVector.push_back(new Transition("ButtonPress", "StateOpening"));
 	states.insert(make_pair("StateClosed", new State("StateClosed", stateClosedTransitionVector)));
 
+	/*
 	//Opening
 	vector<Transition*> stateOpeningTransitionVector(3);
 	stateOpeningTransitionVector.push_back(new Transition("ButtonPress", "StatePausedOpening"));
@@ -42,12 +42,14 @@ void Context::setupStateTable() {
 	vector<Transition*> statePausedOpeningTransitionVector(1);
 	statePausedOpeningTransitionVector.push_back(new Transition("ButtonPress", "StateClosing"));
 	states.insert(make_pair("StatePausedOpening", new State("StatePausedOpening", statePausedOpeningTransitionVector)));
+	*/
 
 	//Open
 	vector<Transition*> stateOpenTransitionVector(1);
 	stateOpenTransitionVector.push_back(new Transition("ButtonPress", "StateClosing"));
 	states.insert(make_pair("StateOpen", new State("StateOpen", stateOpenTransitionVector)));
 
+	/*
 	//Closing
 	vector<Transition*> stateClosingTransitionVector(4);
 	stateClosingTransitionVector.push_back(new Transition("FullyClosed", "StateClosed"));
@@ -60,6 +62,7 @@ void Context::setupStateTable() {
 	vector<Transition*> statePausedClosingTransitionVector(1);
 	statePausedClosingTransitionVector.push_back(new Transition("ButtonPress", "StateOpening"));
 	states.insert(make_pair("StatePausedClosing", new State("StatePausedClosing", statePausedClosingTransitionVector)));
+	*/
 
 	Context::stateTable.states = states;
 	Context::stateTable.currentState = "StateClosed";
@@ -68,50 +71,66 @@ void Context::setupStateTable() {
 /*
 void * Context::run(void *arg) {
 	while(1) {
+		if (!contextQueue.empty()){
+			Event event = contextQueue.pop();
+			acceptEvent(event);
+		}
+		if (stateTable.currentState.equals("StateOpening")){
+			position++;
+			if (position == 10){
+				stateTable.acceptEvent(new Event("o", "FullyOpen"));
+			}
+		}
+		if (stateTable.currentState.equals("StateClosing")){
+			position--;
+			if (position == 0) {
+				stateTable.acceptEvent(new Event("c", "FullyClosed"));
+			}
+		}
 
+		usleep(999999);
 	}
 }
 */
 
-void Context::acceptEvent(Event event) {
-	if (event.eventName == "ButtonPress") {
-		BUTTON_PRESSED = true;
-		OVERCURRENT = false;
-		INFRARED_BEAM = false;
-
-		//If the door is in either of these states and the button is pressed, the door should close.
-		string statesToCloseFrom[2] = {"StateOpen", "StatePausedOpening"};
-		for (int i = 0; i < 2; i++) {
-			if (statesToCloseFrom[i] == stateTable.currentState) {
-				Context::toRaise = false;
-				break;
-			}
+// Note: This function only exists as a backup to expected functionality. If the system is fully
+//		 functional, then this function will be deleted. All it does is move the door up and down.
+//		 It does not handle threading, nor does it allow for interrupts. Little Johnny just needs
+//		 to be careful if he's walking under a closing garage door.
+void Context::moveDoor(bool opening) {
+	if (opening) {
+		int count = 1;
+		while (count <= 10) {
+			cout << count << endl;
+			usleep(999999);
+			count++;
 		}
-
-		//See above comment, but instead the door should open.
-		string statesToOpenFrom[2] = {"StateClosed", "StatePausedClosing"};
-		for (int i = 0; i < 2; i++) {
-			if (statesToOpenFrom[i] == stateTable.currentState) {
-				Context::toRaise = true;
-				break;
-			}
+	} else {
+		int count = 10;
+		while (count >= 1) {
+			count << count << endl;
+			usleep(999999);
+			count--;
 		}
-
-		pthread_t timerThread;
-		pthread_create(&timerThread, NULL, Context::startTimer, NULL);
-	} else if (event.eventName == "Overcurrent") {
-		BUTTON_PRESSED = false;
-		OVERCURRENT = true;
-		INFRARED_BEAM = false;
-	} else if (event.eventName == "InfraredBeam") {
-		BUTTON_PRESSED = false;
-		OVERCURRENT = false;
-		INFRARED_BEAM = true;
 	}
-
-	stateTable.acceptEvent(event);
 }
 
+void Context::acceptEvent(Event event) {
+	//if (event.eventName == "ButtonPress") {
+	//	pthread_t timerThread;
+	//	pthread_create(&timerThread, NULL, Context::startTimer, NULL);
+	//}
+
+	//TODO: Keep this like this, if it doesn't work, change to just check current state.
+	//		If closed, count to 10 then open. If open, vice versa.
+	stateTable.acceptEvent(event);
+	if (stateTable.currentState = "StateClosed") {
+		Context::moveDoor(true);
+	} else {
+		Context::moveDoor(false);
+	}
+}
+/*
 void * Context::startTimer(void *arg) {
 	//NOTE: This is based on the assumption that the timer thread and input threads don't lock each
 	//		other out. If this proves to not be the case, the mutex soltution will be required.
@@ -146,3 +165,4 @@ void * Context::startTimer(void *arg) {
 	Context::motorUp = false;
 	Context::motorDown = false;
 }
+*/
--- Context.hpp
+++ Context.hpp
@@ -1,5 +1,6 @@
 #include "Event.hpp"
 #include "StateTable.hpp"
+#include <queue>
 
 class Context {
 	public:
@@ -9,11 +10,13 @@ class Context {
 		static int position;
 		static bool toRaise;
 		static StateTable stateTable;
+		static queue<Event> contextQueue;
 
 		Context();
 
 		static void setupStateTable();
 		//static void * run(void* arg);
+		void moveDoor(bool);
 		void acceptEvent(Event);
 		static void * startTimer(void* arg);
 };
